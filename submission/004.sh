6bf755c350f0aa59756e1afec0161264aac7abaa
Túlio Albuquerque <thulle777@gmail.com>
2024-11-23 20:41:26 -0300

    fix: jq format in submission 004

--- submission/004.sh
+++ submission/004.sh
@@ -2,5 +2,5 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 pubkey="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 info=$(bitcoin-cli getdescriptorinfo "tr($pubkey/100)" | jq -r ".descriptor")
-derive=$(bitcoin-cli deriveaddresses "$info" | jq -r".[]")
+derive=$(bitcoin-cli deriveaddresses "$info" | jq -r ".[]")
 echo $derive
